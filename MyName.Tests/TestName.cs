f72d32600a9f23d63fc2099cf1df31deea5b59fb
yossatron <yossatron@yossatrons-MacBook-Pro.local>
2018-04-03 15:44:52 +0700

    Create Test NameLanguage

--- MyName.Tests/TestName.cs
+++ MyName.Tests/TestName.cs
@@ -5,12 +5,37 @@ namespace Yossatron.MyName.Tests
 {
     public class TestName
     {
-        [Fact]
+        private Name myName;
+        public TestName(){
+            myName = new Name();
+        }
+
+         [Fact]
         public void TestGetNameShoudReturnMyName()
         {
-            Name myNmae = new Name();
-            string result = myNmae.Get();
+            string result = myName.Get();
             Assert.Equal("Yossatron", result);
+        }
+
+        [Fact]
+        public void TestGetNameWithoutLanguageShoudReturnMyNameInEnglish()
+        {
+            string result = myName.Get("e");
+            Assert.Equal("Yossatron", result);
+        }
+         [Fact]
+        public void TestGetNameWithoutLanguageShoudReturnMyNameInThailand()
+        {
+            string result = myName.Get("t");
+            Assert.Equal("ยศธร", result);
+        }
+        [Theory] /// รันตามลำดับ line เรียงจากบนไปล่าง 
+        [InlineData("e","Yossatron")]
+        [InlineData("t","ยศธร")]
+        public void TestGetNameWithoutThaiLanguageShoudReturnMyNameInThai(string inputLanguage,string expectedResult){
+            string result = myName.Get(inputLanguage);
+            Assert.Equal(expectedResult,result);
+
         }
     }
 }
--- MyName/Name.cs
+++ MyName/Name.cs
@@ -4,8 +4,22 @@ namespace Yossatron.MyName
 {
     public class Name
     {
-       public string Get() {
+        public string Get(string lang = "e")
+        {
+            if("e".Equals(lang)){
+                return GetEnglish();
+            }
+            return GetThai();
+        }
+
+        private string GetThai()
+        {
+            return "ยศธร";
+        }
+        private string GetEnglish()
+        {
             return "Yossatron";
         }
+
     }
 }
